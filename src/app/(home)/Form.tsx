85e1554ee5072a3de2e4a6ffb72edc529fe2f00e
Juan David Serna <J.Serna@blueline-laundry.com>
2024-06-25 20:46:48 +1000

    Working now omn Form

--- src/app/(home)/Form.tsx
+++ src/app/(home)/Form.tsx
@@ -4,8 +4,12 @@ import React from 'react'
 
 export default function Form() {
   return (
-    <section>
-        <form>
+    <section className='page bg-gray-100'>
+        <form 
+          className='relative page border shadow ' 
+          onSubmit={() => console.log('submited')}
+        >
+          <h3 className='font-bold text-center text-blueBranding text-[1.3rem] '>Get a quote</h3>
         </form>
     </section>
   )
