e51c51f26ae5691cd4c85e1c125be2d6e0755659
Juan David Serna <J.Serna@blueline-laundry.com>
2024-06-28 21:25:47 +1000

    Form is now sending values to Bk

--- amplify/backend/function/handleGeneralQuotes/src/index.js
+++ amplify/backend/function/handleGeneralQuotes/src/index.js
@@ -1,6 +1,18 @@
 // AWS Region
 const REGION = "ap-southeast-2"
 
+// import Template
+const mjml2html = require('mjml')
+
+const { SendEmailCommand } = require("@aws-sdk/client-ses")
+const { SESClient } = require("@aws-sdk/client-ses")
+
+// Create SES service object
+const sesClient = new SESClient({ region: REGION })
+
+// Image url
+const imgUrl = "https://blueline-cleaning.s3.ap-southeast-2.amazonaws.com/logo1.png"
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
@@ -9,13 +21,127 @@ exports.handler = async (event) => {
     const parsedBody = JSON.parse(event.body)
 
     console.log('parsed', parsedBody)
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-     headers: {
-         "Access-Control-Allow-Origin": "*",
-         "Access-Control-Allow-Headers": "*"
-     },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+
+    try {
+        const resToBlueline = await sendMailToBlueline(parsedBody)
+        console.log('sending mail to blueline', resToBlueline)
+
+        if(resToBlueline) {
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "*"
+                },
+                body:JSON.stringify({ status: 200 })
+            }
+        } else {
+            throw new Error('sending failed')
+        }
+
+    } catch (error) {
+        console.error('error', error)
+        
+        return {
+            statusCode: 400,
+            body: 'Sending failed'
+        }
+    }
 };
+
+
+// Function that sends email to Ausadvent
+const sendMailToBlueline = async(body) => {
+    const mjmlTemplateToBlueline = `
+        <mjml>
+            <mj-head>
+                <mj-title>Your quote request</mj-title>
+                <mj-font name="Quantico" href="https://fonts.googleapis.com/css2?family=Inter&display=swap" />
+                <mj-attributes>
+                    <mj-text font-family="Quantico, sans-serif" />
+                <mj-class name="phone" />
+                </mj-attributes>
+            </mj-head>
+            <mj-body>
+                <mj-section>
+                    <mj-column>
+                        <mj-image src=${imgUrl} alt="Ausadvent logo" align="center" width="79px" padding="0"></mj-image>
+                    </mj-column>
+                </mj-section>
+                <mj-section> 
+                    <mj-divider border-color="#007FA3" border-width="1px"></mj-divider>
+                </mj-section>
+                <mj-section padding-top="32px" padding-left="16px" padding-right="16px" padding-bottom="16px" >
+                    <mj-text font-size="16px">Hello Management team, <br/><br/><br/></mj-text>
+                    <mj-text font-size="16px">The person ${body.values.firstname} ${body.values.lastname} wants to get a quote for a ${body.values.service} service and left the following service description:<br/><br/></mj-text>
+                    <mj-text font-size="16px">"${body.values.description}"<br/><br/></mj-text>
+                    <mj-text font-size="16px">His/her email address is ${body.values.email} and contact number ${body.values.phoneNumber}. <br/><br/></mj-text>
+                
+                    <mj-text font-size="16px" >Best regards,<br/><br/></mj-text>
+                    <mj-text font-size="16px" >Blueline Cleaning.<br/><br/></mj-text>
+                    
+                    <mj-section padding-left="40px" padding-right="30px"> 
+                        <mj-divider border-color="#007FA3" border-width="1px"></mj-divider>
+                    </mj-section>
+                    <mj-section padding="0">
+                        <mj-wrapper padding="0" display="flex"  >
+                            <mj-column mj-class="phone" vertical-align="middle">
+                            <mj-text color="#2563EB" align="center"><a href="tel:+61428994431" style="text-decoration: none; "> M: +61 0428 994 431 </a> </mj-text>
+                                <mj-text align="center">59-61 Creek Road, New Town, Tas 7008</mj-text>
+                            </mj-column>
+                        </mj-wrapper>
+                    </mj-section>
+                </mj-section>
+            </mj-body>
+        </mjml>
+    `;
+
+    // Convert MJML to HTML
+    const { html } = mjml2html(mjmlTemplateToBlueline);
+
+    const createSendEmailCommand = (toAddress, fromAddress) => {
+        return new SendEmailCommand({
+            Destination: {
+                CcAddresses: [],
+                ToAddresses: [
+                    toAddress
+                ]
+            },
+            Message: {
+                Body: {
+                    /* required */
+                    Html: {
+                      Charset: "UTF-8",
+                      Data: html,
+                    },
+                    Text: {
+                      Charset: "UTF-8",
+                      Data: `The person ${body.values.firstName}`,
+                    },
+                },
+                Subject: {
+                    Charset: "UTF-8",
+                    Data: "Your quote request",
+                },
+            },
+            Source: fromAddress
+        })
+    }
+
+    const sendEmailCommand = createSendEmailCommand(
+        // Change it for the CX email address
+        "sernadominguezj@gmail.com",
+        `j.serna@blueline-laundry.com`,
+    );
+
+    try {
+        const resultForBlueline = await sesClient.send(sendEmailCommand)
+        console.log('Result mail to cx', resultForBlueline);
+
+        return {
+            statusCode: 200
+        }
+    } catch (error) {
+        console.error('error', error)
+    }
+}
--- src/app/(home)/Form.tsx
+++ src/app/(home)/Form.tsx
@@ -1,18 +1,21 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useFormik } from 'formik'
 import * as Yup from "yup"
+import Lottie from 'lottie-react'
 import Image from 'next/image'
 
 // Assets
 import Logo from '../../../assets/blue-logo.png'
 import Indicate from '../../../assets/form-image.png'
+import Success from '../../../assets/success.json'
 
 // Amplify configuration
 import config from '@/amplifyconfiguration.json'
 import { Amplify } from 'aws-amplify'
 import { post } from 'aws-amplify/api'
+
 
 // Api key
 const apiKey: string | undefined = process.env.NEXT_PUBLIC_API_KEY;
@@ -30,13 +33,25 @@ Amplify.configure(config, {
 
 export default function Form() {
   
+  // State Hooks
+  const [buttonText, setButtonText] = useState("Submit")
+  const [isSending, setIsSending] = useState(false)
+
+  // State to render or hide form when success sending message
+  const [renderForm, setRenderForm] = useState(true);
+  const [renderSuccessMessage, setRenderSuccessMessage] = useState(false);
+
+  // State to render error when sending message
+  const [renderErrorMessage, setRenderErrorMessage] = useState(false);
+
   // Functions
   const formik = useFormik({
     initialValues: {
       firstname: "",
       lastname: "",
       email: "",
-      service: "",
+      phoneNumber: "",
+      service: "Commercial cleaning",
       description: "",
       terms: ""
     }, 
@@ -50,12 +65,17 @@ export default function Form() {
       email: Yup.string()
         .email("Invalid email address")
         .required("Email address is required"),
+      phoneNumber: Yup.string()
+        .min(10, "Please provide a valid phone number")
+        .required("Phone number is required"),
       description: Yup.string()
         .min(20, "Please leave us a more detailed description about your premises")
         .required('A premises description is required'),
       terms: Yup.array().required("Terms of service must be checked")
     }),
     onSubmit: async(values) => {
+      setButtonText('Sending...')
+      setIsSending(true)
       try {
         let resOperation = post({
           apiName: 'generalQuotes',
@@ -63,112 +83,147 @@ export default function Form() {
           options: {
             body: {
               values
-            },
-            headers: {
-              Authorization: ""
             }
           }
         })
 
         if((await resOperation.response).statusCode == 200) {
-          console.log('sent to bk')
+          setRenderForm(!renderForm)
+          setRenderSuccessMessage(true)
+          setTimeout(() => {
+            setButtonText("Message sent")
+            setIsSending(false)
+          }, 2000)
         } else {
-          console.log('message not sent')
+          setRenderForm(false)
+          setRenderErrorMessage(true)
+          setTimeout(() => {
+            setButtonText('Message not sent')
+            setIsSending(false)
+          }, 2000)
         }
       } catch (error) {
-        console.log(error)
+        console.log("error", error)
+        setRenderForm(false)
+        setRenderErrorMessage(true)
+        setTimeout(() => {
+          // Reset the button text and indicate that the form has been sent
+          setButtonText('Message not sent');
+          setIsSending(false);
+        }, 4000);
       }
     }
   })
 
   return (
     <section className='px-[1rem] py-[3rem] sm:mx-auto sm:px-[2rem] md:px-[4rem] lg:px-[8rem] bg-blueBranding' id='contact' >
-        <form 
-          className='relative page 2xl:w-3/4 2xl:mx-auto lg:px-[4rem] border shadow-xl bg-gradient-to-tl from-gray-200 to-white lg:flex ' 
-          onSubmit={formik.handleSubmit}
-        >
-          <div className="lg:w-1/3 xl:w-1/4 flex flex-col justify-center items-center gap-[2rem]">
-            <Image 
-              src={Logo} 
-              className='h-[8rem] lg:h-[10rem] w-auto  '
-              title='Blueline Logo'
-              alt='Blueline Logo'
-            />
-            <Image 
-              src={Indicate}
-              className='hidden lg:block ml-[5rem] w-[25rem] opacity-80 '
-              title='Blueline worker'
-              alt='Blueline worker inviting to fill the form'
-            />
+        {renderForm && (
+          <form 
+            className='relative page 2xl:w-3/4 2xl:mx-auto lg:px-[4rem] border shadow-xl bg-gradient-to-tl from-gray-200 to-white lg:flex ' 
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="lg:w-1/3 xl:w-1/4 flex flex-col justify-center items-center gap-[2rem]">
+              <Image 
+                src={Logo} 
+                className='h-[8rem] lg:h-[10rem] w-auto  '
+                title='Blueline Logo'
+                alt='Blueline Logo'
+              />
+              <Image 
+                src={Indicate}
+                className='hidden lg:block ml-[5rem] w-[25rem] opacity-80 '
+                title='Blueline worker'
+                alt='Blueline worker inviting to fill the form'
+              />
 
-          </div>
-          <div className="page lg:px-0 flex flex-col gap-[1rem]">
-            <h3 className='font-bold text-center text-blueBranding text-[1.5rem] '>Get a quote</h3>
-            {/* Firstname */}
-            <label htmlFor="firstname" className='mt-[1rem] flex flex-col gap-[0.5rem]'>
-              <span className={`${formik.touched.firstname && formik.errors.firstname ? 'text-red-400' : 'text-blueBranding'}`}>
-                {formik.touched.firstname && formik.errors.firstname 
-                  ? formik.errors.firstname 
-                  : 'First name'
-                }
-              </span>
-              <input 
+            </div>
+            <div className="page lg:px-0 flex flex-col gap-[1rem]">
+              <h3 className='font-bold text-center text-blueBranding text-[1.5rem] '>Get a quote</h3>
+              {/* Firstname */}
+              <label htmlFor="firstname" className='mt-[1rem] flex flex-col gap-[0.5rem]'>
+                <span className={`${formik.touched.firstname && formik.errors.firstname ? 'text-red-400' : 'text-blueBranding'}`}>
+                  {formik.touched.firstname && formik.errors.firstname 
+                    ? formik.errors.firstname 
+                    : 'First name'
+                  }
+                </span>
+                <input 
+                    required 
+                    type="text" 
+                    name='firstname' 
+                    value={formik.values.firstname} 
+                    onChange={formik.handleChange} 
+                    placeholder='Enter your name' 
+                    onBlur={formik.handleBlur}
+                    className='form-input '
+                />
+              </label>
+
+              {/* Last */}
+              <label htmlFor="lastname" className='flex flex-col gap-[0.5rem]'>
+                <span className={`${formik.touched.lastname && formik.errors.lastname ? 'text-red-400' : 'text-blueBranding'}`}>
+                  {formik.touched.lastname && formik.errors.lastname 
+                    ? formik.errors.lastname 
+                    : 'Last name'
+                  }
+                </span>
+                <input 
+                    required 
+                    type="text" 
+                    name='lastname' 
+                    value={formik.values.lastname} 
+                    onChange={formik.handleChange} 
+                    placeholder='Enter your last name' 
+                    onBlur={formik.handleBlur}
+                    className='form-input '
+                />
+              </label>
+
+              {/* Email */}
+              <label htmlFor="email" className='flex flex-col gap-[0.5rem]'>
+                <span className={`${formik.touched.email && formik.errors.email ? 'text-red-400' : 'text-blueBranding'}`}>
+                  {formik.touched.email && formik.errors.email 
+                    ? formik.errors.email 
+                    : 'Email'
+                  }
+                </span>
+                <input 
                   required 
-                  type="text" 
-                  name='firstname' 
-                  value={formik.values.firstname} 
+                  type='email' 
+                  name='email' 
+                  value={formik.values.email} 
                   onChange={formik.handleChange} 
-                  placeholder='Enter your name' 
+                  placeholder='Enter your email address' 
                   onBlur={formik.handleBlur}
-                  className='form-input '
-              />
-            </label>
+                  className='form-input'
+                />
+              </label>
 
-            {/* Last */}
-            <label htmlFor="lastname" className='flex flex-col gap-[0.5rem]'>
-              <span className={`${formik.touched.lastname && formik.errors.lastname ? 'text-red-400' : 'text-blueBranding'}`}>
-                {formik.touched.lastname && formik.errors.lastname 
-                  ? formik.errors.lastname 
-                  : 'Last name'
-                }
-              </span>
-              <input 
+              {/* Phone */}
+              <label htmlFor="phoneNumber" className='flex flex-col gap-[0.5rem]'>
+                <span className={`${formik.touched.phoneNumber && formik.errors.phoneNumber ? 'text-red-400' : 'text-blueBranding'}`}>
+                  {formik.touched.phoneNumber && formik.errors.phoneNumber 
+                    ? formik.errors.phoneNumber 
+                    : 'Phone number'
+                  }
+                </span>
+                <input 
                   required 
-                  type="text" 
-                  name='lastname' 
-                  value={formik.values.lastname} 
+                  type="tel" 
+                  name='phoneNumber' 
+                  value={formik.values.phoneNumber} 
                   onChange={formik.handleChange} 
-                  placeholder='Enter your last name' 
+                  placeholder='Enter your phone number' 
                   onBlur={formik.handleBlur}
-                  className='form-input '
-              />
-            </label>
+                  className='form-input'
+                />
+              </label>
 
-            {/* Email */}
-            <label htmlFor="email" className='flex flex-col gap-[0.5rem]'>
-              <span className={`${formik.touched.email && formik.errors.email ? 'text-red-400' : 'text-blueBranding'}`}>
-                {formik.touched.email && formik.errors.email 
-                  ? formik.errors.email 
-                  : 'Email'
-                }
-              </span>
-              <input 
-                required 
-                type='email' 
-                name='email' 
-                value={formik.values.email} 
-                onChange={formik.handleChange} 
-                placeholder='Enter your email address' 
-                onBlur={formik.handleBlur}
-                className='form-input'
-              />
-            </label>
-
-            {/* Service */}
-            <label htmlFor="service" className='flex flex-col gap-[0.5rem]'>
+              {/* Service */}
+              <label htmlFor="service" className='flex flex-col gap-[0.5rem]'>
                 <span className='text-blueBranding'>Service type</span>
                   <div className=''>
-                    <select name="service " value={formik.values.service} onChange={formik.handleChange} className='bg-transparent rounded w-full px-0 text-black'>
+                    <select name="service" value={formik.values.service} onChange={formik.handleChange} className='bg-transparent rounded w-full px-0 text-black'>
                       <option className='w-[8rem]' >Commercial cleaning</option>
                       <option className='w-[8rem]'> Industrial cleaning</option>
                       <option className='w-[8rem]'>Factory cleaning</option>
@@ -176,52 +231,67 @@ export default function Form() {
                       <option className='w-[8rem]'>School cleaning</option>
                     </select>
                   </div>
-            </label>
+              </label>
 
-            {/* Description */}
-            <label htmlFor="description" className='flex flex-col gap-[0.5rem]'>
-              <span className={`${formik.touched.description && formik.errors.description ? 'text-red-400' : 'text-blueBranding'}`}>
-                  {formik.touched.description && formik.errors.description 
-                    ? formik.errors.description 
-                    : 'Service description'
-                  }
-              </span>
-              <textarea 
-                required   
-                name='description' 
-                value={formik.values.description} 
-                onChange={formik.handleChange} 
-                placeholder='Please let us know your ideas for your project and how we can help you' 
-                onBlur={formik.handleBlur}
-                className='min-h-[12rem] p-2 bg-white border border-gray-300 '
-              />
-            </label>
-
-            {/* Terms */}
-            <label htmlFor="terms">
-              <span className='text-black'>Terms of service</span>
-              <div className='flex gap-[1rem] items-center  '>
-                <input 
-                    required
-                    type='checkbox' 
-                    name='terms' 
-                    onChange={formik.handleChange} 
-                    className='' 
+              {/* Description */}
+              <label htmlFor="description" className='flex flex-col gap-[0.5rem]'>
+                <span className={`${formik.touched.description && formik.errors.description ? 'text-red-400' : 'text-blueBranding'}`}>
+                    {formik.touched.description && formik.errors.description 
+                      ? formik.errors.description 
+                      : 'Service description'
+                    }
+                </span>
+                <textarea 
+                  required   
+                  name='description' 
+                  value={formik.values.description} 
+                  onChange={formik.handleChange} 
+                  placeholder='Please let us know your ideas for your project and how we can help you' 
+                  onBlur={formik.handleBlur}
+                  className='min-h-[12rem] p-2 bg-white border border-gray-300 '
                 />
-                <p className='text-black text-[0.7rem] md:text-[0.8rem]'>I consent to be contacted by Blueline in response to the information provided in this form.</p>
-              </div>
-            </label>
+              </label>
 
-            {/* Button */}
-            <button 
-              className='mt-[1rem] relative mx-auto px-2 lg:px-4 py-1 lg:p-2 bg-blueBranding font-bold text-white w-1/2 flex justify-center text-center ' 
-              type='submit'
-              // onClick={}
-            >
-              Submit
-            </button>
+              {/* Terms */}
+              <label htmlFor="terms">
+                <span className='text-black'>Terms of service</span>
+                <div className='flex gap-[1rem] items-center  '>
+                  <input 
+                      required
+                      type='checkbox' 
+                      name='terms' 
+                      onChange={formik.handleChange} 
+                      className='' 
+                  />
+                  <p className='text-black text-[0.7rem] md:text-[0.8rem]'>I consent to be contacted by Blueline in response to the information provided in this form.</p>
+                </div>
+              </label>
+
+              {/* Button */}
+              <button 
+                className='mt-[1rem] relative mx-auto px-2 lg:px-4 py-1 lg:p-2 bg-blueBranding font-bold text-white w-1/2 flex justify-center text-center ' 
+                type='submit'
+                // onClick={}
+              >
+                {buttonText}
+              </button>
+            </div>
+          </form>
+        )}
+
+         {/* Render succes animation */}
+         {!renderForm && renderSuccessMessage && (
+          <div className='flex flex-col  items-center'>
+            <Lottie 
+              className='w-[12rem] lg:w-[26rem] xl:w-[30rem] '
+              animationData={Success}
+              loop={true}
+            />
+            {/* <div className='border-b-2 border-pink-200 w-1/3'></div> */}
+            <h3 className='mt-[2rem] text-white text-[1.25rem] xl:text-[1.875rem] font-semibold'>Your message has been sent!</h3>
+            <p className='text-white'>Our team will get in touch shortly.</p>
           </div>
-        </form>
+         )}
     </section>
   )
 }
